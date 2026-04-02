374853cdad5f917bf099c3c7a18cecc76a7ffeac
Qvarnström Jacob <jacob.qvarnstrom@keolis.se>
2026-04-02 15:54:22 +0200

    Updated year to be dynamic, fixed encoding

--- TheGrunkGames/Program.cs
+++ TheGrunkGames/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using TheGrunkGames.Hubs;
 using TheGrunkGames.Services;
 
@@ -12,12 +14,18 @@ namespace TheGrunkGames
     {
         public static async Task Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             var builder = WebApplication.CreateBuilder(args);
             builder.AddServiceDefaults();
             builder.AddMongoDBClient("thegrunkgames");
 
             builder.Services.AddProblemDetails();
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
+                });
             builder.Services.AddSingleton<MatchmakingService>();
             builder.Services.AddSingleton<IStorageService, StorageService>();
             builder.Services.AddSingleton<IGameService, GameService>();
